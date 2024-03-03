8941b395007cc6e66a8c2508fd30a94be6144ef5
spatel555 <66096325+spatel333@users.noreply.github.com>
2024-03-03 16:59:28 -0500

    menu.py started; extract, tranform, load scripts initialized but empty

--- ETL-Template/menu.py
+++ ETL-Template/menu.py
@@ -1,11 +1,25 @@
+import extract
 import transform
+import load
 
 def main():
     script_name = __file__.split('/')[-1]
-
     print(f'{script_name}')
 
+    print("Welcome to the menu. Let's check our data")
+    login()
+    
     transform.main()
+
+def login():
+    credentials_file = "/path/to/file"
+    
+    # check time of last credentials update & advise password change
+
+    # if empty, prompy for creds
+    # username = input("Username")
+    # password = input("Password")
+
 
 
 
