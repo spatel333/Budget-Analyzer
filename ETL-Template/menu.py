5796339b718199b4acc1806f05149dad3a04b131
spatel555 <66096325+spatel333@users.noreply.github.com>
2024-03-10 13:56:10 -0400

    Moving grab_credentials() & login() into login.py This cleans up menu.py

--- ETL-Template/menu.py
+++ ETL-Template/menu.py
@@ -1,8 +1,6 @@
-from pathlib import Path
 import extract
 import transform
 import load
-import subprocess
 import csv
 
 
@@ -17,46 +15,6 @@ def main():
     print("\n\n")
     print("Welcome to the menu. Let's check our data")
     login()
-    print("")
-
-def login():
-    while(True):
-        prompt = input("Want to update credentials? [y/n]: ")
-        if prompt.lower() == 'y':
-            grab_credentials()
-        elif prompt.lower() == 'n':
-            print("Using existing credentials")
-            break
-
-
-
-    ### TODO:
-        # Make an API call to bank site
-        # Acknowledge Successful Login
-    msg = "Here we'd be implementing an API Call to the bank institution"
-    [print('*') for i in len(msg)]
-    print(msg)
-    [print('*') for i in len(msg)]
-    ###
-
-    
-
-    print("Login Complete!")
-
-
-def grab_credentials():
-
-    # open file in which we'll save credentials
-    credentials_path = "./credentials.txt"
-    subprocess.run(['touch', credentials_path])
-
-    # prompt for credentials
-    username = input("Username: ")
-    password = input("Password: ")
-
-    with open(credentials_path, 'w') as creds_file:
-        creds_file.write(f'{username}\n{password}')
-
 
 
 
