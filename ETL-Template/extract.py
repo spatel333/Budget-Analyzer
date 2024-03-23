b5d6c17d31add8ada7684ab0c7b991af074093e5
spatel333 <shahilpatel99@gmail.com>
2024-03-23 09:53:16 -0400

    Merged conflicts

--- ETL-Template/extract.py
+++ ETL-Template/extract.py
@@ -1,34 +1,8 @@
-import subprocess
+def main():
+    script_name = __file__.split('/')[-1]
 
-def login():
-    while(True):
-        prompt = input("Want to update credentials? [y/n]: ")
-        if prompt.lower() == 'y':
-            grab_credentials()
-        elif prompt.lower() == 'n':
-            print("Using existing credentials")
-            break
+    print(f'{script_name}')
 
-    ### TODO:
-        # Make an API call to bank site
-        # Acknowledge Successful Login
-    query_site()
-
-    print("Login Complete!")
-
-
-def grab_credentials():
-
-    # open file in which we'll save credentials
-    credentials_path = "./credentials.txt"
-    subprocess.run(['touch', credentials_path])
-
-    # prompt for credentials
-    username = input("Username: ")
-    password = input("Password: ")
-
-    with open(credentials_path, 'w') as creds_file:
-        creds_file.write(f'{username}\n{password}')
 
 def query_site():
 
@@ -50,6 +24,4 @@ def load_local_data():
 
 
 if __name__ == "__main__":
-    script_name = __file__.split('/')[-1]
-    print(f'{script_name}')
-    [print(f'\t{x}') for x in locals().values() if callable(x)]
+    main()
