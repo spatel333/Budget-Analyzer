432db9f753c5ac6e634ad5eeb3ceb7463cb56343
spatel333 <shahilpatel99@gmail.com>
2024-03-28 16:29:56 -0400

    Implemented taxation methods. Print net winnings after taxes (not accounting for the deduction when taking a lump sum)
    Need to incorporate this into budget analyzer

--- ETL-Template/hit_the_lottery.py
+++ ETL-Template/hit_the_lottery.py
@@ -9,12 +9,27 @@ Now the question is... where do we put this money?
 """
 
 def main():
+    # pull jackpot amount
     gross_winnings = pull_current_lotto_winnings()
-    print(f'Gross Winnings: {gross_winnings}')
-
-    gross_winnings_as_currency = winnings_as_currency(gross_winnings)
+    gross_winnings_as_currency = decimal_as_currency(gross_winnings)
     print(f'Congrats on winning {gross_winnings_as_currency}')
-    # taxes = pull_taxation(gross_winnings)
+    
+    # calculate taxes on gross winnings
+    taxes, tax_breakdown = tax_winnings(gross_winnings)
+    taxes_as_currency = decimal_as_currency(taxes)
+
+    # print tax information
+    print(f'You will pay {taxes_as_currency} in taxes')
+    yes_responses = {'y', 'Y', 'yes', 'YES'}
+    if input("Would you like a breakdown? [Y/n]: ") in yes_responses:
+        [print(f'\t{key}:\t${value}') for key,value in tax_breakdown.items()]
+    print("-------------------------------------")
+
+    # print net winnings
+    net_winnings_as_decimal = gross_winnings - taxes
+    net_winnings_as_currency = decimal_as_currency(net_winnings_as_decimal)
+    print(f'Your net winnings is {net_winnings_as_currency}')
+
 
 def pull_current_lotto_winnings() -> Decimal:
     # url = "https://data.ny.gov/resource/d6yy-54nr.json"
@@ -31,10 +46,10 @@ def pull_current_lotto_winnings() -> Decimal:
     amount_as_currency = results.text[results.text.find('$'):]
     amount_as_decimal = Decimal(sub(r'[^\d.]', '', amount_as_currency))
     
-    return amount_as_decimal
+    return round(amount_as_decimal, 2)
     
 
-def winnings_as_currency(gross_winnings: int) -> str:
+def decimal_as_currency(gross_winnings: int) -> str:
     
     #set locale data 
     locale.setlocale(locale.LC_ALL, 'en_US.utf8')
@@ -43,9 +58,17 @@ def winnings_as_currency(gross_winnings: int) -> str:
     return locale.currency( gross_winnings, grouping=True)
 
 
+def tax_winnings(amount) -> tuple:
 
-def pull_taxation(amount):
-    return amount*0.2
+    # calculate each taxation amount
+    tax_breakdown = {}
+    tax_breakdown['Federal'] = round(amount * Decimal(0.24), 2)
+    tax_breakdown['State'] = round(amount * Decimal(0.05), 2)
+    
+    # calculate gross taxation amount
+    taxes = tax_breakdown['Federal'] + tax_breakdown['State']
+
+    return round(taxes, 2), tax_breakdown
 
 
 
