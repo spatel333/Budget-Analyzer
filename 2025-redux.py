aff53a7ecec8d16bd0564ad2d79cb84a0aa2fe13
spatel555 <66096325+spatel333@users.noreply.github.com>
2025-02-03 07:05:28 -0500

    import csv

--- 2025-redux.py
+++ 2025-redux.py
@@ -1,6 +1,6 @@
 import os, sys
 import pandas as pd
-import pdfplumber
+import pdfplumber, csv
 
 # Define standard headers
 STANDARD_HEADERS = ["Name", "Date", "Amount"]
@@ -27,6 +27,30 @@ def extract_text_from_pdf(path_to_pdf):
 
     return extracted_data
 
+def extract_text_from_csv(path_to_csv=None):
+    """
+    Extract data from CSV
+    """
+
+    # print(path_to_csv)
+    with open(path_to_csv, "r") as csv_file:
+        # print(csv_file)
+
+        # creating a csvreader object
+        csvreader = csv.reader(csv_file)
+
+        # extract field names (from first row)
+        fields = next(csvreader)
+
+        print(fields)
+
 if __name__ == "__main__":
-    print(f"filepath: {sys.argv[1]}")
-    print("Hello World")
+
+    all_raw_filenames = os.listdir("./extract/raw_files/")
+
+    absolute_filepaths_prefix =\
+        os.path.abspath('.') + "/extract/raw_files/"
+    
+    for file in all_raw_filenames:
+        extract_text_from_csv(absolute_filepaths_prefix + file)
+        break
